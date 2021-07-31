e29ccd9a415f0d43b013ac4c13357cf8aa8076d7
nabila hajou <nab.ikk13@gmail.com>
2021-07-31 11:53:54 +0200

    ajout d'un mousemove

---
+++ index.js
@@ -0,0 +1,11 @@
+const mousemove = document.querySelector('.mousemove');
+
+
+window.addEventListener('mousemove', (e) => 
+{
+
+mousemove.style.top = e.pageY + "px"
+mousemove.style.left = e.pageX + "px"
+
+
+})
